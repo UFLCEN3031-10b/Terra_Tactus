36722e76f3316e3c03c8a43606b916b66af00f98
Mark Turiansky <mark.turiansky@yahoo.com>
2015-10-01 03:59:20 +0000

    General housekeeping commit

--- config/env/development.js
+++ config/env/development.js
@@ -4,7 +4,7 @@ var defaultEnvConfig = require('./default');
 
 module.exports = {
   db: {
-    uri: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://meanapp:meanapp@ds027749.mongolab.com:27749/cen3031-fall-test/mean-dev' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/mean-dev',
+    uri: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://USERNAME:PASSWORD@ds045622.mongolab.com:45622/terratactus',
     options: {
       user: '',
       pass: ''
