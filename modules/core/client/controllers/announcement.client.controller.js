3369ff827182aa2b64a543f350304df2257947ab
Mark Turiansky <mark.turiansky@yahoo.com>
2015-10-01 05:20:34 +0000

    [#104212644] added controller for announcements

---
+++ modules/core/client/controllers/announcement.client.controller.js
@@ -0,0 +1,9 @@
+'use strict';
+
+angular.module('core').controller('AnnouncementController', ['$scope', function ($scope) {
+        //this needs to be populated with a call to the database
+        //to receive the most recent announcements
+        //posted by the admin account
+        $scope.announcement = [{text:'testing...'}];
+    }
+]);
