da5582bddd32efd6664b43fedcdaa11de6385015
barahonaraul <barahonaraul@live.com>
2015-10-06 11:46:53 -0400

    Update products.client.controller.js
    The one with more dummy data....

--- modules/users/client/controllers/products.client.controller.js
+++ modules/users/client/controllers/products.client.controller.js
@@ -11,7 +11,14 @@ angular.module('core').controller('ProductsController', function ($scope) {
         title: 'Exploring Gator Football',
         image: 'http://www.gatortailgating.com/files/imagecache/gt7_full_580/mike/2012/03/gators-usf.jpg',
         text: 'Sample Text Mauris non dui sit amet magna volutpat suscipit sed eu mi. Nam molestie, justo id egestas vulputate, leo mauris elementum turpis, eget mollis ipsum nibh et metus. Vestibulum nec ligula non felis laoreet blandit ac id erat.'
-    
+    },{
+        title: 'Taking a Look at Century Tower',
+        image: 'http://cdn.webservices.ufhealth.org/wp-content/blogs.dir/350/files/2012/05/UFCampus.jpg',
+        text: 'Sample Text Mauris non dui sit amet magna volutpat suscipit sed eu mi. Nam molestie, justo id egestas vulputate, leo mauris elementum turpis, eget mollis ipsum nibh et metus. Vestibulum nec ligula non felis laoreet blandit ac id erat.'
+    },{
+        title: 'Its Great to be a Florida Gator!',
+        image: 'http://thumb.usatodaysportsimages.com/image/thumb/650-510nw/8805035.jpg',
+        text: 'Florida, our Alma Mater, thy glorious name we praise. All thy loyal sons and daughters, a joyous song shall raise. Where palm and pine are blowing, where southern seas are flowing, Shine forth thy noble gothic walls, thy lovely vine clad halls.Neath the orange and blue victorious,our love shall never fail. Theres no other name so glorious,all hail, Florida, hail!'
     }];
 
     $scope.addProduct = function (newTitle,newImage, newText) {
