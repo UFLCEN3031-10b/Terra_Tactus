a652f6eb659b121c3d10c10c9ffdb14dfffc8196
Mark Turiansky <mark.turiansky@yahoo.com>
2015-10-08 05:23:32 +0000

    [#104212644] modified homepage controller to query api

--- modules/core/client/controllers/home.client.controller.js
+++ modules/core/client/controllers/home.client.controller.js
@@ -1,8 +1,15 @@
 'use strict';
 
-angular.module('core').controller('HomeController', ['$scope', 'Authentication',
-  function ($scope, Authentication) {
+angular.module('core').controller('HomeController', ['$scope', '$http', 'Authentication',
+  function ($scope, $http, Authentication) {
     // This provides Authentication context.
     $scope.authentication = Authentication;
+
+    $http.get('/api/homepage-data').success(function (res) {
+        if (res === null) console.log('[ERROR] homepage-data does not exist yet.');
+        else {
+            console.log(res.aboutUs);
+        }
+    });
   }
 ]);
