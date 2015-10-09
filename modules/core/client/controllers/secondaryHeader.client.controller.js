fe389e4a0b9b3aebc03fca24534a4ee9e42817db
Mark Turiansky <mark.turiansky@yahoo.com>
2015-10-09 06:16:18 +0000

    [#104212644] added authentication to controller, will be needed

--- modules/core/client/controllers/secondaryHeader.client.controller.js
+++ modules/core/client/controllers/secondaryHeader.client.controller.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('core').controller('SecondaryHeaderController', ['$scope', function ($scope) {
-        $scope.test = '';
+angular.module('core').controller('SecondaryHeaderController', ['$scope', 'Authentication', function ($scope, Authentication) {
+        $scope.authentication = Authentication;
     }
 ]);
