6c735335deac08ac8d399ce7d2352536090c7802
Mark Turiansky <mark.turiansky@yahoo.com>
2015-10-09 04:19:44 +0000

    [#104212644] updated contact data controller and view

--- modules/core/client/controllers/contact.client.controller.js
+++ modules/core/client/controllers/contact.client.controller.js
@@ -1,6 +1,21 @@
 'use strict';
 
 angular.module('core').controller('ContactController', ['$scope', '$http', function ($scope, $http) {
-        $scope.test = '';
+        $scope.contactName = '';
+        $scope.address = '';
+        $scope.citystatezip = '';
+        $scope.phone = '';
+        $scope.fax = '';
+
+        $http.get('/api/contact-info').success(function (res) {
+            if (res === null) console.log('[ERROR] contact-info does not exist yet.');
+            else {
+                $scope.contactName = res.name;
+                $scope.address = res.address;
+                $scope.citystatezip = res.citystatezip;
+                $scope.phone = res.phone;
+                $scope.fax = res.fax;
+            }
+        });
     }
 ]);
