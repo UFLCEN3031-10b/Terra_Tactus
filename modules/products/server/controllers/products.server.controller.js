5cfd73ebee321497ac6f8f6450a816849887fe49
Mark Turiansky <mark.turiansky@yahoo.com>
2015-10-20 05:25:37 +0000

    updated products server directory

---
+++ modules/products/server/controllers/products.server.controller.js
@@ -0,0 +1,5 @@
+'use strict';
+
+var path = require('path').
+    mongoose = require('mongoose'),
+    errorHandler = require(path.resolve('./modules/core/server/controllers/errors.server.controller'));
