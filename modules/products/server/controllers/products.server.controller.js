fa54de2c9b13a1888733714592aeadaa43b46861
Mark Turiansky <mark.turiansky@yahoo.com>
2015-10-20 05:39:28 +0000

    minor bug fix, added logo to navbar

--- modules/products/server/controllers/products.server.controller.js
+++ modules/products/server/controllers/products.server.controller.js
@@ -1,5 +1,5 @@
 'use strict';
 
-var path = require('path').
+var path = require('path'),
     mongoose = require('mongoose'),
     errorHandler = require(path.resolve('./modules/core/server/controllers/errors.server.controller'));
