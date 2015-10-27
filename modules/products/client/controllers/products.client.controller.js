8076da63a3e927398de3d2deff32b784b09d37cb
barahonaraul <barahonaraul@live.com>
2015-10-27 15:01:10 -0400

    Working on db implementation, added policy folder with policy file, modified models, working on controller and routes
    # Conflicts:
    #	modules/products/server/routes/products.server.routes.js

--- modules/products/client/controllers/products.client.controller.js
+++ modules/products/client/controllers/products.client.controller.js
@@ -61,22 +61,4 @@ angular.module('core').controller('geoProductsController', function ($scope) {
 });
 
 
-angular.module('core').controller('TabsCtrl', ['$scope', function ($scope) {
-    $scope.tabs = [{
-            title: 'Cultural Products',
-            url: 'one.tpl.html'
-        }, {
-            title: 'Geological Products',
-            url: 'two.tpl.html'
-    }];
 
-    $scope.currentTab = 'one.tpl.html';
-
-    $scope.onClickTab = function (tab) {
-        $scope.currentTab = tab.url;
-    };
-    
-    $scope.isActiveTab = function(tabUrl) {
-        return tabUrl === $scope.currentTab;
-    };
-}]);
--- modules/products/server/controllers/products.server.controller.js
+++ modules/products/server/controllers/products.server.controller.js
@@ -2,4 +2,55 @@
 
 var path = require('path'),
     mongoose = require('mongoose'),
+ //   products = mongoose.model('products'),
     errorHandler = require(path.resolve('./modules/core/server/controllers/errors.server.controller'));
+/*
+exports.find = function (req, res) {
+    products.findOne().exec(function (err, data) {
+        if (err) {
+            return res.status(400).send({
+                message: errorHandler.getErrorMessage(err)
+            });
+        } else {
+            res.json(data);
+        }
+    });
+};
+
+exports.update = function (req, res) {
+    products.findOne().exec(function (err, data) {
+        if (err) {
+            return res.status(400).send({
+                message: errorHandler.getErrorMessage(err)
+            });
+        } else {
+            data.remove(function (err) {
+                if (err) {
+                    return res.status(400).send({
+                        message: errorHandler.getErrorMessage(err)
+                    });
+                }
+            });
+        }
+    });
+
+    var d = new products();
+    d.proType = req.body.proType;
+    d.proTitle = req.body.proTitle;
+    d.longDes = req.body.longDes;
+    d.shortDes = req.body.shortDes;
+    d.imageUrl = req.body.imageUrl;
+    d.imageSet = req.body.imageSet;
+    d.priceSet = req.body.priceSet;
+
+    d.save(function (err) {
+        if (err) {
+            return res.status(400).send({
+                message: errorHandler.getErrorMessage(err)
+            });
+        } else {
+            res.json(d);
+        }
+    });
+};
+*/
