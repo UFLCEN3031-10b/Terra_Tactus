12e3010cf55609cc815149a343f5aad64018a8e4
barahonaraul <barahonaraul@live.com>
2015-10-30 14:21:48 -0400

    Improved short description text block for products list, added a comment to the product model for future refrence on proType

--- modules/products/server/models/product.server.model.js
+++ modules/products/server/models/product.server.model.js
@@ -4,7 +4,7 @@ var mongoose = require('mongoose'),
     Schema = mongoose.Schema;
 
 var ProductSchema = new Schema({
-    proType: Boolean,
+    proType: Boolean, //false = geological, true = cultural
     proTitle: {
         type: String,
         default: '',
