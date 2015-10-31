a56682739794846f3252d62397c4785d8f32795a
dlarson94 <damian.larson@ufl.edu>
2015-10-31 17:26:34 -0400

    minor html adjustments

--- modules/users/client/controllers/verify.client.controller.js
+++ modules/users/client/controllers/verify.client.controller.js
@@ -1,14 +1,17 @@
 'use strict';
 
 angular.module('users').controller('VerifyController', ['$scope', '$state', '$http', 'Authentication',
-  function($scope, $http, $state, Authentication){
+  function($scope, $state, $http, Authentication){
     $scope.verify = false;
     $scope.user = Authentication.user;
 
     $scope.submit = function (isValid) {
-      if(isValid){
-        console.log("Submit called successfully!");
+      if(!isValid){
+        $scope.$broadcast('show-errors-check-validity', 'userForm');
+        return false;
       }
+
+      console.log("Submitted successfully!");
     };
   }
 ]);
