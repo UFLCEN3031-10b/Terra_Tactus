df4cd09c3f9d93962a3227decb08d921dabe9749
dlarson94 <damian.larson@ufl.edu>
2015-10-31 17:28:35 -0400

    deleted broken route

--- modules/users/server/routes/auth.server.routes.js
+++ modules/users/server/routes/auth.server.routes.js
@@ -18,7 +18,6 @@ module.exports = function (app) {
   app.route('/api/auth/signup').post(users.signup);
   app.route('/api/auth/signin').post(users.signin);
   app.route('/api/auth/signout').get(users.signout);
-  app.route('/api/auth/verify').post(users.verify);
 
   // Setting the facebook oauth routes
   app.route('/api/auth/facebook').get(users.oauthCall('facebook', {
