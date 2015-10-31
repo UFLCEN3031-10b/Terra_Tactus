beed2d1444a9541e34f13a9e605f96b99fdb023e
dlarson94 <damian.larson@ufl.edu>
2015-10-30 20:46:43 -0400

    added inputs, ensured validation

--- modules/users/client/controllers/verify.client.controller.js
+++ modules/users/client/controllers/verify.client.controller.js
@@ -1,7 +1,14 @@
 'use strict';
 
-angular.module('users').controller('VerifyController', ['$scope', '$state', '$http',
-  function($scope, $http, $state){
+angular.module('users').controller('VerifyController', ['$scope', '$state', '$http', 'Authentication',
+  function($scope, $http, $state, Authentication){
     $scope.verify = false;
+    $scope.user = Authentication.user;
+
+    $scope.submit = function (isValid) {
+      if(isValid){
+        console.log("Submit called successfully!");
+      }
+    };
   }
 ]);
