5728241d1bf119dc5596640ff9ba681dd6494490
Mark Turiansky <mark.turiansky@yahoo.com>
2015-11-10 06:26:37 +0000

    setting up new views

--- modules/payment/client/config/payment.client.routes.js
+++ modules/payment/client/config/payment.client.routes.js
@@ -9,5 +9,16 @@ angular.module('payment').config(['$stateProvider', function ($stateProvider) {
         .state('order-complete', {
             url: '/order/complete/:orderId',
             templateUrl: 'modules/payment/client/views/complete.client.view.html'
+        })
+        .state('user-orders', {
+            url: '/order/open',
+            templateUrl: 'modules/payment/client/views/open.client.view.html'
+        })
+        .state('admin-orders', {
+            url: '/order/admin',
+            templateUrl: 'modules/payment/client/views/admin-orders.client.view.html',
+            data: {
+                roles: ['admin']
+            }
         });
 }]);
