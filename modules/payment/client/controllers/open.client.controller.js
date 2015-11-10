5759fb17a078f6e33bec4cd18c904250eaa8c6ad
Mark Turiansky <mark.turiansky@yahoo.com>
2015-11-10 22:39:15 +0000

    updating individual user open order views

--- modules/payment/client/controllers/open.client.controller.js
+++ modules/payment/client/controllers/open.client.controller.js
@@ -1,7 +1,11 @@
 'use strict';
 
 angular.module('payment').controller('UserOrderController', ['$scope', '$http', function ($scope, $http) {
+    $scope.isLoading = true;
+    $scope.orders = [];
+
     $http.get('/api/order/list').success(function (res) {
-        console.log(res);
+        $scope.isLoading = false;
+        $scope.orders = res;
     });
 }]);
