210d882196e7261c68bdf630f77ace9915133f30
Mark Turiansky <mark.turiansky@yahoo.com>
2015-11-11 19:17:44 +0000

    setting up admin view

--- modules/payment/client/controllers/admin-orders.client.controller.js
+++ modules/payment/client/controllers/admin-orders.client.controller.js
@@ -1,5 +1,22 @@
 'use strict';
 
-angular.module('payment').controller('AdminOrderController', ['$scope', function ($scope) {
+angular.module('payment').controller('AdminOrderController', ['$scope', '$http', function ($scope, $http) {
+    $scope.activeState = 0;
+    $scope.isLoading = true;
+    $scope.orders = [];
 
+    $scope.setActive = function (n) {
+        $scope.isLoading = true;
+        $scope.orders = [];
+        $scope.activeState = n;
+        $http.get('/api/order/list').success(function (res) {
+            $scope.orders = res;
+            $scope.isLoading = false;
+        });
+    };
+
+    $http.get('/api/order/list').success(function (res) {
+        $scope.orders = res;
+        $scope.isLoading = false;
+    });
 }]);
