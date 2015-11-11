9a0289c4a54820ddb18bfebea8d9b406d1bf0999
kaufmann42 <kaufmann43@gmail.com>
2015-11-11 18:17:53 -0500

    minor fixes with ng sanitize

--- config/assets/default.js
+++ config/assets/default.js
@@ -19,8 +19,7 @@ module.exports = {
         'public/lib/angular-ui-utils/ui-utils.js',
         'public/lib/angular-bootstrap/ui-bootstrap-tpls.js',
         'public/lib/angular-file-upload/angular-file-upload.js',
-        'public/lib/owasp-password-strength-test/owasp-password-strength-test.js',
-        'public/lib/angular-sanitize/angular-sanitize.js'
+        'public/lib/owasp-password-strength-test/owasp-password-strength-test.js'
       ],
       tests: ['public/lib/angular-mocks/angular-mocks.js']
     },
