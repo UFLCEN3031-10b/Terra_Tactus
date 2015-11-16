13b3fe6b4dd4110e09e7f00f303086e29b1a2dcd
Mark Turiansky <mark.turiansky@yahoo.com>
2015-11-16 18:19:24 +0000

    setting up admin routes

---
+++ modules/payment/server/controllers/admin-payment.server.controller.js
@@ -0,0 +1,4 @@
+'use strict';
+
+var path = require('path'),
+    errorHandler = require(path.resolve('./modules/core/server/controllers/errors.server.controller'));
---
+++ modules/payment/server/routes/admin-payment.server.routes.js
@@ -0,0 +1,7 @@
+'use strict';
+
+var adminControl = require('../controllers/admin-payment.server.controller');
+
+module.exports = function () {
+    
+};
