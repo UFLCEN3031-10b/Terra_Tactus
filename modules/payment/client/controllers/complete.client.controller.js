d331ec50e8469e942b94aa686efbcc620e864d7f
Michelle Emamdie <memamdie@users.noreply.github.com>
2015-11-23 16:13:25 -0500

    Revert "Paypal integration"

--- modules/payment/client/controllers/complete.client.controller.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-angular.module('payment').controller('OrderCompleteController', ['$scope', '$http', '$stateParams', '$rootScope', '$location', function ($scope, $http, $stateParams, $rootScope, $location) {
-    $rootScope.$broadcast('cartChange');
-
-    $http.delete('/api/order/close/' + $stateParams.orderId).error(function (err) {
-        $location.path('/');
-    });
-}]);
