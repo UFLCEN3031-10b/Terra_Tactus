8e9afe24515d14e1e21917d571475618a7024d8a
Mark Turiansky <mark.turiansky@yahoo.com>
2015-11-30 12:33:05 -0500

    setting up payment tests

---
+++ modules/payment/tests/server/payment.server.routes.tests.js
@@ -0,0 +1,45 @@
+'use strict';
+
+var should = require('should'),
+    request = require('supertest'),
+    path = require('path'),
+    mongoose = require('mongoose'),
+    User = mongoose.model('User'),
+    Order = mongoose.model('Order'),
+    express = require(path.resolve('./config/lib/express'));
+
+var app, agent, credentials, user, order;
+
+describe('Payment CRUD tests', function () {
+    before(function (done) {
+        app = express.init(mongoose);
+        agent = request.agent(app);
+
+        done();
+    });
+
+    beforeEach(function (done) {
+        credentials = {
+            username: 'username4tests',
+            password: 'M3@n.jsI$Aw3$0m3'
+        };
+
+        user = new User({
+            firstName: 'firstName',
+            lastName: 'lastName',
+            displayName: 'firstName lastName',
+            email: 'test@test.com',
+            username: credentials.username,
+            password: credentials.password,
+            provider: 'local'
+        });
+
+        user.save(function () {
+            done();
+        });
+    });
+});
+
+afterEach(function (done) {
+    User.remove().exec(done);
+});
