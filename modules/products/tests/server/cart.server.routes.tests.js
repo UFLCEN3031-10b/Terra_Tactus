53f7a30441f6e96b5374bf03bc86c6f6a9ef1adc
Mark Turiansky <mark.turiansky@yahoo.com>
2015-12-04 17:36:32 -0500

    tests added

--- modules/products/tests/server/cart.server.routes.tests.js
+++ modules/products/tests/server/cart.server.routes.tests.js
@@ -18,7 +18,86 @@ describe('Cart CRUD tests', function () {
         done();
     });
 
-    beforeEach(function (done) {});
+    beforeEach(function (done) {
+        credentials = {
+            username: 'username4tests',
+            password: 'M3@n.jsI$Aw3$0m3'
+        };
 
-    afterEach(function (done) {});
+        user = new User({
+            firstName: 'firstName',
+            lastName: 'lastName',
+            displayName: 'firstName lastName',
+            email: 'test@test.com',
+            username: credentials.username,
+            password: credentials.password,
+            provider: 'local'
+        });
+
+        product = new Product({
+            proTitle: 'test product',
+            shortDes: 'description',
+            indvPrice: '1.00',
+            eduPrice: '2.00',
+            wholePrice: '3.00'
+        });
+
+        user.save(function () {
+            product.save(done);
+        });
+    });
+
+    it('should allow you to add a product when not logged in', function (done) {
+        agent.post('/api/cart/product/' + product._id)
+            .expect(200)
+            .end(function (err, res) {
+                if (err) {
+                    return done(err);
+                }
+
+                should(res).be.ok();
+                done();
+            });
+    });
+
+    it('should allow you to add a product when logged in', function (done) {
+        agent.post('/api/auth/signin')
+            .send(credentials)
+            .expect(200)
+            .end(function () {
+                agent.post('/api/cart/product/' + product._id)
+                    .expect(200)
+                    .end(function (err, res) {
+                        if (err) {
+                            return done(err);
+                        }
+
+                        should(res).be.ok();
+                        done();
+                    });
+            });
+    });
+
+    it('should respond with the length of the cart', function (done) {
+        agent.get('/api/cart/length')
+            .expect(200)
+            .end(function (err, res) {
+                if (err) {
+                    return done(err);
+                }
+
+                should(JSON.parse(res.text)).have.property('length');
+                done();
+            });
+    });
+
+    afterEach(function (done) {
+        User.remove().exec(function () {
+            Product.remove().exec(function () {
+                agent.delete('/api/cart')
+                    .expect(200)
+                    .end(done);
+            });
+        });
+    });
 });
