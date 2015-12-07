8352da71b6979491ba4de572ee6dcddb7714fe56
kaufmann42 <kaufmann43@gmail.com>
2015-12-06 23:29:59 -0500

    set up testing files

--- config/env/test.js
+++ config/env/test.js
@@ -4,10 +4,10 @@ var defaultEnvConfig = require('./default');
 
 module.exports = {
   db: {
-    uri: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/mean-test',
+    uri: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://ds053718.mongolab.com:53718/terratactus',
     options: {
-      user: '',
-      pass: ''
+      user: 'use',
+      pass: 'use'
     },
     // Enable mongoose debug mode
     debug: process.env.MONGODB_DEBUG || false
