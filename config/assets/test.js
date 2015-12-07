888bfc4763b14041e9156f6509d84342d8870804
memamdie <memamdie@ufl.edu>
2015-12-07 13:41:49 -0500

    reset test back to proper way

--- config/assets/test.js
+++ config/assets/test.js
@@ -2,8 +2,8 @@
 
 module.exports = {
   tests: {
-    //client: ['modules/*/tests/client/**/*.js'],
+    client: ['modules/*/tests/client/**/*.js'],
     server: ['modules/*/tests/server/*.js'],
-    //e2e: ['modules/*/tests/e2e/**/*.js']
+    e2e: ['modules/*/tests/e2e/**/*.js']
   }
 };
