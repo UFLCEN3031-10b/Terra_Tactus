f8749ded0b9fb114929feb290d1fa0541dcc6b6d
barahonaraul <barahonaraul@live.com>
2015-12-09 16:51:34 -0500

    Added more working test!

--- modules/products/tests/server/products.server.routes.tests.js
+++ modules/products/tests/server/products.server.routes.tests.js
@@ -49,54 +49,88 @@ describe('Product CRUD tests', function () {
 
     });
 
-    //Test # 1 to see if you can access the products without being signed in as a user.
+    //Test # 1 to see if you can access the products when not signed in
     it('Should attempt to access products without being signed in', function(done){
         agent.get('/api/products')
             .expect(200)
             .end(done);
     });
 
-    //Test # 2 to see if you can create products without being signed in.
-    it('Should expect forbidden when creating a product without being signed in', function(done){
+    //Test # 2 to see if you can get a list of products when not signed in
+    it('should be able to get a list of products if not signed in', function (done) {
+      // Create new product model instance
+      var productObj = new Product(product);
+
+      // Save the product
+      productObj.save(function () {
+        // Request products
+        request(app).get('/api/products')
+          .end(function (req, res) {
+            // Set assertion
+            res.body.should.be.instanceof(Array).and.have.lengthOf(1);
+
+            // Call the assertion callback
+            done();
+          });
+
+      });
+    });
+
+    //Test # 3 to see if you can create products without being signed in.
+    it('Should not be able to create a product without being signed in', function(done){
         agent.post('/api/products')
             .send(product)
             .expect(403)
             .end(done);
     });
 
-    //Test # 3 to see if you can create products when signed in as an admin.
-    it('Should be able to create a product when signed in as an admin', function(done){
-      //user.roles = ['admin'];
-      //user.save(function () {
+    //Test #4 stest to see that a product is saved when signed in as an admin
+    it('should be able to save a product if logged in as an admin', function (done) {
+      //note our user was created with admin as its roles attribute
       agent.post('/api/auth/signin')
-          .send(credentials)
-          .expect(200)
-          .end(function (signinErr, signinRes) {
-            // Handle signin error
-            if (signinErr) {
-              return done(signinErr);
-            }
+        .send(credentials)
+        .expect(200)
+        .end(function (signinErr, signinRes) {
+          // Handle signin error
+          if (signinErr) {
+            return done(signinErr);
+          }
 
-              agent.post('/api/products')
-                  .send(product)
-                  .expect(200)
-                  .end(function (err, res) {
-                      if (err) {
-                          return done(err);
-                      }
+          // Save a new product
+          agent.post('/api/products')
+            .send(product)
+            .expect(200)
+            .end(function (productSaveErr, productSaveRes) {
+              // Handle product save error
+              if (productSaveErr) {
+                return done(productSaveErr);
+              }
 
-                      should(res).be.ok();
-                      done();
-                  });
-          });
+              // Get a list of products
+              agent.get('/api/products')
+                .end(function (productsGetErr, productsGetRes) {
+                  // Handle products save error
+                  if (productsGetErr) {
+                    return done(productsGetErr);
+                  }
 
-      //  });
+                  // Get products list
+                  var products = productsGetRes.body;
 
+                  // Set assertions
+                  (products[0].proTitle).should.match('test product');
+
+                  // Call the assertion callback
+                  done();
+                });
+            });
+        });
     });
 
-    //Test # 4 to see if you can create products when signed in as a free user.
+
+    //Test # 5 to see if you can create products when signed in as a free user.
     it('Should not be able to create a product when signed in a free user', function(done){
-      user.roles = ['freeUser'];
+      user.roles = ['freeUser']; //we change our user role to freeUser and save
       user.save(function () {
       agent.post('/api/auth/signin')
           .send(credentials)
@@ -106,7 +140,7 @@ describe('Product CRUD tests', function () {
             if (signinErr) {
               return done(signinErr);
             }
-
+              //attempt to save a product
               agent.post('/api/products')
                   .send(product)
                   .expect(403)
@@ -119,7 +153,91 @@ describe('Product CRUD tests', function () {
                       done();
                   });
 
-                });
+              });
+        });
+    });
+
+    //Test # 6 to see is a product can be saved without a title
+    it('should not be able to save an product if no title is provided', function (done) {
+      // Invalidate title field
+      product.proTitle = '';
+
+      agent.post('/api/auth/signin')
+        .send(credentials)
+        .expect(200)
+        .end(function (signinErr, signinRes) {
+          // Handle signin error
+          if (signinErr) {
+            return done(signinErr);
+          }
+
+          // Save a new product
+          agent.post('/api/products')
+            .send(product)
+            .expect(400)
+            .end(function (productSaveErr, productSaveRes) {
+              // Set message assertion
+              (productSaveRes.body.message).should.match('Product must have a name');
+
+              // Handle product save error
+              done(productSaveErr);
+            });
+        });
+    });
+
+    //Test # 7 to see is a product can be saved without a short description
+    it('should not be able to save an product if no shortDes is provided', function (done) {
+      // Invalidate field
+      product.shortDes = '';
+
+      agent.post('/api/auth/signin')
+        .send(credentials)
+        .expect(200)
+        .end(function (signinErr, signinRes) {
+          // Handle signin error
+          if (signinErr) {
+            return done(signinErr);
+          }
+
+          // Save a new product
+          agent.post('/api/products')
+            .send(product)
+            .expect(400)
+            .end(function (productSaveErr, productSaveRes) {
+              // Set message assertion
+              (productSaveRes.body.message).should.match('Product must have a short description');
+
+              // Handle product save error
+              done(productSaveErr);
+            });
+        });
+    });
+
+    //Test # 8 to see is a product can be saved without a long description
+    it('should not be able to save an product if no longDes is provided', function (done) {
+      // Invalidate field
+      product.longDes = '';
+
+      agent.post('/api/auth/signin')
+        .send(credentials)
+        .expect(200)
+        .end(function (signinErr, signinRes) {
+          // Handle signin error
+          if (signinErr) {
+            return done(signinErr);
+          }
+
+          // Save a new product
+          agent.post('/api/products')
+            .send(product)
+            .expect(400)
+            .end(function (productSaveErr, productSaveRes) {
+              // Set message assertion
+              (productSaveRes.body.message).should.match('Product must have a long description');
+
+              // Handle product save error
+              done(productSaveErr);
+            });
         });
     });
 
