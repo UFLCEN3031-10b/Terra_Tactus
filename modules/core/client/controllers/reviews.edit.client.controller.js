5d00351694fbe513cf3c1cdbe2c513cfbaafa1f2
memamdie <memamdie@ufl.edu>
2015-12-09 13:24:26 -0500

    renamed files from reviews to testimonials

--- modules/core/client/controllers/reviews.edit.client.controller.js
+++
@@ -1,3 +0,0 @@
-/**
- * Created by memamdie on 12/8/15.
- */
---
+++ modules/core/client/controllers/testimonial.client.controller.js
@@ -0,0 +1,86 @@
+/**
+ * Created by memamdie on 12/8/15.
+ */
+'use strict';
+
+angular.module('core').controller('TestimonialController', function ($scope, $http) {
+    $http.get('/api/testimonial/data').success(function (res) {
+        console.log(res);
+    });
+    $http.get('/api/testimonial/data').success(function (res) {
+        console.log(res);
+        $scope.testimonials = res;
+    });
+
+
+    $scope.addTestimonial = function (newFrom ,newQuote, newPictureURL, newCreditURL) {
+        $scope.products.push({
+            from: newFrom,
+            quote: newQuote,
+            pictureUrl: newPictureURL,
+            creditUrl: newCreditURL
+        });
+    };
+    //// Create new Product
+    //$scope.create = function (isValid) {
+    //    //Declare variables for correctly getting checkbox values
+    //    var prodType = null;
+    //    var teachType = null;
+    //    $scope.error = null;
+    //    //Check if our producForm was valid, if not create function is canceled and errors show on GUI
+    //    if (!isValid) {
+    //        $scope.$broadcast('show-errors-check-validity', 'productForm');
+    //        return false;
+    //    }
+    //    //set the correct value of the product type variable
+    //    if(document.getElementById("proType-cb").checked) {
+    //        prodType = true;
+    //    }
+    //    else {
+    //        prodType = false;
+    //    }
+    //    //set the correct value of the teacher variable
+    //    if(document.getElementById("teacher-cb").checked) {
+    //        teachType = true;
+    //    }
+    //    else {
+    //        teachType = false;
+    //    }
+    //
+    //    // Create new Product object
+    //    var product = new Products({
+    //        proType: prodType,
+    //        proTitle: this.proTitle,
+    //        longDes: this.longDes,
+    //        shortDes: this.shortDes,
+    //        imageUrl: this.imageUrl,
+    //        imageOne: this.imageOne,
+    //        imageTwo: this.imageTwo,
+    //        imageThree: this.imageThree,
+    //        imageFour: this.imageFour,
+    //        indvPrice: this.indvPrice,
+    //        eduPrice: this.eduPrice,
+    //        wholePrice: this.wholePrice,
+    //        teacher: teachType,
+    //        features: this.tempFeatures,
+    //        curriculum: this.tempTable
+    //    });
+    //
+    //    // Redirect after save
+    //    product.$save(function (response) {
+    //        $location.path('products');
+    //
+    //    }, function (errorResponse) {
+    //        $scope.error = errorResponse.data.message;
+    //    });
+    //
+    //    console.log('Product has been created');
+    //};
+
+    // Find a list of Products
+    //$scope.find = function () {
+    //    $scope.testimonials = Testimonials.query();
+    //};
+
+
+});
