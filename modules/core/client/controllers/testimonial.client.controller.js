dfbc0ff6e86bb22bb2c6a28a294406ddce99a6a1
memamdie <memamdie@ufl.edu>
2015-12-11 13:23:12 -0500

    changed testimonial client controllers

--- modules/core/client/controllers/testimonial.client.controller.js
+++ modules/core/client/controllers/testimonial.client.controller.js
@@ -1,75 +1,50 @@
-/**
- * Created by memamdie on 12/8/15.
- */
 'use strict';
 
-angular.module('core').controller('TestimonialController', function ($scope, $http) {
+// Testimonials controller
+angular.module('core').controller('TestimonialsController', ['$window','$http','$scope','$rootScope', '$stateParams', '$location', 'Authentication', 'Testimonials',
+    function ($window, $http, $scope, $rootScope, $stateParams, $location, Authentication, Testimonials) {
+        $scope.authentication = Authentication;
 
-    $http.get('/api/testimonial/data').success(function (res) {
-        console.log(res);
-        $scope.testimonialData = res;
-    });
+        // Create new Testimonial
+        $scope.create = function (isValid) {
+            $scope.error = null;
+            //Check if our producForm was valid, if not create function is canceled and errors show on GUI
+            if (!isValid) {
+                $scope.$broadcast('show-errors-check-validity', 'testimonialDataForm');
+                return false;
+            }
 
-    //// Create new Product
-    //$scope.create = function (isValid) {
-    //    //Declare variables for correctly getting checkbox values
-    //    var prodType = null;
-    //    var teachType = null;
-    //    $scope.error = null;
-    //    //Check if our producForm was valid, if not create function is canceled and errors show on GUI
-    //    if (!isValid) {
-    //        $scope.$broadcast('show-errors-check-validity', 'productForm');
-    //        return false;
-    //    }
-    //    //set the correct value of the product type variable
-    //    if(document.getElementById("proType-cb").checked) {
-    //        prodType = true;
-    //    }
-    //    else {
-    //        prodType = false;
-    //    }
-    //    //set the correct value of the teacher variable
-    //    if(document.getElementById("teacher-cb").checked) {
-    //        teachType = true;
-    //    }
-    //    else {
-    //        teachType = false;
-    //    }
-    //
-    //    // Create new Product object
-    //    var product = new Products({
-    //        proType: prodType,
-    //        proTitle: this.proTitle,
-    //        longDes: this.longDes,
-    //        shortDes: this.shortDes,
-    //        imageUrl: this.imageUrl,
-    //        imageOne: this.imageOne,
-    //        imageTwo: this.imageTwo,
-    //        imageThree: this.imageThree,
-    //        imageFour: this.imageFour,
-    //        indvPrice: this.indvPrice,
-    //        eduPrice: this.eduPrice,
-    //        wholePrice: this.wholePrice,
-    //        teacher: teachType,
-    //        features: this.tempFeatures,
-    //        curriculum: this.tempTable
-    //    });
-    //
-    //    // Redirect after save
-    //    product.$save(function (response) {
-    //        $location.path('products');
-    //
-    //    }, function (errorResponse) {
-    //        $scope.error = errorResponse.data.message;
-    //    });
-    //
-    //    console.log('Product has been created');
-    //};
+            // Create new Testimonial object
+            var testimonial = new Testimonials({
+                from: this.from,
+                quote: this.quote,
+                pictureUrl: this.pictureUrl,
+                creditUrl: this.creditUrl
+            });
 
-    // Find a list of Products
-    //$scope.find = function () {
-    //    $scope.testimonials = Testimonials.query();
-    //};
+            // Redirect after save
+            testimonial.$save(function (response) {
+                $location.path('testimonials');
+
+            }, function (errorResponse) {
+                $scope.error = errorResponse.data.message;
+            });
+
+            console.log('Testimonial has been created');
+        };
+
+        // Find a list of Testimonials
+        $scope.find = function () {
+            $scope.testimonials = Testimonials.query();
+        };
+
+        // Find existing Testimonials
+        $scope.findOne = function () {
+            $scope.testimonial = Testimonials.get({
+                testimonialId: $stateParams.testimonialId
+            });
+        };
 
 
-});
+    }
+]);
--- modules/core/client/controllers/testimonial.edit.client.controller.js
+++ modules/core/client/controllers/testimonial.edit.client.controller.js
@@ -2,7 +2,58 @@
 
 angular.module('core').controller('TestimonialEditController', ['$window','$http','$scope','$rootScope', '$stateParams', '$location', 'Authentication', 'Testimonials',
     function ($window, $http, $scope, $rootScope, $stateParams, $location, Authentication, Testimonials) {
+        $scope.authentication = Authentication;
+
+        //Code for deleting a testimonial
+        $scope.delete = function (testimonialID) {
+            var conf = confirm("Are you sure you want to delete this testimonial?");
+            console.log(conf);
+            if(conf){
+                $http.delete('/api/testimonials/' + testimonialID).success(function (res) {
+                    for (var i in $scope.testimonials) {
+                        if ($scope.testimonials[i]._id === testimonialID) {
+                            $scope.testimonials.splice(i, 1);
+                        }
+                    }
+                });
+            }
+        };
 
 
+        // Find existing Testimonials
+        $scope.findOne = function () {
+            $scope.testimonial = Testimonials.get({
+                testimonialId: $stateParams.testimonialId
+            });
+        };
 
-    }]);
+        //
+        //Code to Update Testimonial
+        $scope.updateTest = function (edited_testimonial,isValid) {
+            //Check if the updateTestimonialForm is valid, if not cancel update and display errors
+            if (!isValid) {
+                $scope.$broadcast('show-errors-check-validity', 'updateTestimonialForm');
+                return false;
+            }
+            //update testimonial
+            edited_testimonial.$update(function () {
+            }, function (errorResponse) {
+                $scope.error = errorResponse.data.message;
+            });
+            //redirect to general testimonials page
+            $location.path('testimonials');
+
+        };
+
+        //If we cancel an edit, redirect to the general edit page
+        $scope.cancelEdit = function(){
+            $location.path('testimonials-edit');
+        };
+
+        //Get all the testimonials
+        $http.get('/api/testimonials').success(function (res) {
+            $scope.testimonials = res;
+        });
+
+    }
+]);
