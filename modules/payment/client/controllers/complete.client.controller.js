7131dc700560a6e33ae8f520b0cbc845b66fc067
Mark Turiansky <mark.turiansky@yahoo.com>
2015-12-10 20:02:11 -0500

    order complete comment

--- modules/payment/client/controllers/complete.client.controller.js
+++ modules/payment/client/controllers/complete.client.controller.js
@@ -1,8 +1,10 @@
 'use strict';
 
 angular.module('payment').controller('OrderCompleteController', ['$scope', '$http', '$stateParams', '$rootScope', '$location', function ($scope, $http, $stateParams, $rootScope, $location) {
+    // tell the system that the cart has updated
     $rootScope.$broadcast('cartChange');
 
+    // make sure to close the order so it can't be accessed again
     $http.delete('/api/order/close/' + $stateParams.orderId).error(function (err) {
         $location.path('/');
     });
