5556bc74d21863d2b4382dd7a9d9c2d49574702d
Mark Turiansky <mark.turiansky@yahoo.com>
2015-12-12 00:55:27 -0500

    core client comments

--- modules/core/client/controllers/secondaryHeader.client.controller.js
+++ modules/core/client/controllers/secondaryHeader.client.controller.js
@@ -3,9 +3,15 @@
 angular.module('core').controller('SecondaryHeaderController', ['$scope', '$location', 'Authentication', function ($scope, $location, Authentication) {
         $scope.authentication = Authentication;
 
+        // redirect to the search page on click
         $scope.searchButton = function () {
+            // calculate search string
             var s = (!$scope.searchValue) ? '' : $scope.searchValue;
+
+            // redirect to search with string as the query
             $location.path('/search').search('q',s);
+
+            // reset the value to an empty string when done
             $scope.searchValue = '';
         };
     }
