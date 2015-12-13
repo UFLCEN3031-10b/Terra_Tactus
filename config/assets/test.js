e5fb21592d9bb9326b2c2c63c9c82a2a73274ad4
barahonaraul <barahonaraul@live.com>
2015-12-13 01:56:45 -0500

    Revert test.js

--- config/assets/test.js
+++ config/assets/test.js
@@ -2,8 +2,8 @@
 
 module.exports = {
   tests: {
-    client: ['modules/products/tests/client/**/*.js'],
-    server: ['modules/products/tests/server/**/products.*.js'],
-    //e2e: ['modules/*/tests/e2e/**/*.js']
+    client: ['modules/*/tests/client/**/*.js'],
+    server: ['modules/*/tests/server/**/*.js'],
+    e2e: ['modules/*/tests/e2e/**/*.js']
   }
 };
