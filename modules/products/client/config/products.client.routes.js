fa509520703228bb6f310e552d767786821aa589
dlarson94 <damian.larson@ufl.edu>
2015-12-16 09:55:43 -0500

    got rid of unnecessary code

--- modules/products/client/config/products.client.routes.js
+++ modules/products/client/config/products.client.routes.js
@@ -93,11 +93,6 @@ angular.module('products').config(['$stateProvider',
                     roles: ['admin']
                 }
             })
-            .state('upload', {
-              url: '/upload',
-              templateUrl: 'modules/products/client/views/upload.client.view.html',
-              controller: 'UploadController'
-            })
 
             //adding an under construction page
             .state('construction', {
